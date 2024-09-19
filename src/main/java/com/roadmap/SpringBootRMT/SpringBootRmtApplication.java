b398a3eac76e56a862130c0eec49d96cdea712da
Bharath Sivaraman <bharathsivagiri2001@gmail.com>
2024-09-19 10:00:19 +0530

    Initial commit

---
+++ src/main/java/com/roadmap/SpringBootRMT/SpringBootRmtApplication.java
@@ -0,0 +1,13 @@
+package com.roadmap.SpringBootRMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRmtApplication.class, args);
+	}
+
+}
